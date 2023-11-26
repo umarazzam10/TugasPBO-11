362a3a4ae338bb334450fc9868735dba2d2b9f1c
umarazzam10 <145319150+umarazzam10@users.noreply.github.com>
2023-11-26 22:46:54 +0700

    Add files via upload

---
+++ Tugas 11 PBO/src/DataPelanggan.java
@@ -0,0 +1,18 @@
+public class DataPelanggan implements CetakStruk {
+    String faktur;
+    String nama;
+    String namaBarang;
+    Integer hargaBarang;
+    Integer jmlBarang;
+    Integer totalBayar;
+
+    @Override
+    public void Struk() {
+    }
+
+    //method untuk mengisi data pelanggan
+    public void isiDataPelanggan() {
+    }
+
+    
+}
